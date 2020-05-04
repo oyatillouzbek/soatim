851a68eb91d7433229edbb1f65e35fb136e4785f
oyatillouzbek <63519552+oyatillouzbek@users.noreply.github.com>
2020-05-04 12:47:31 +0500

    Update soat.py

--- soat.py
+++ soat.py
@@ -10,12 +10,6 @@ api_hash = "ebf9b0ec85bde576ae2339974a5cdc41" #my.telegram.org dan olgan apihash
 app = Client("my_account",api_id,api_hash)
 app.start()
 while True:
-    photos = app.get_profile_photos("me")
-    app.delete_profile_photos(photos[0].file_id)
-    im = Image.open("background.jpg")
-    MAX_W, MAX_H = im.size
-    draw = ImageDraw.Draw(im)
-    font = ImageFont.truetype('for.ttf', 501)
     ok = pytz.timezone("Asia/Tashkent")
     x = datetime.datetime.now(tz=ok)
     x = x.strftime("%H:%M")
@@ -23,8 +17,14 @@ while True:
     first_name=str(x),
     about="O'zbekistonda soat: " +str(x)
     ))
+    photos = app.get_profile_photos("me")
+    app.delete_profile_photos(photos[0].file_id)
+    im = Image.open("background.jpg")
+    MAX_W, MAX_H = im.size
+    draw = ImageDraw.Draw(im)
+    font = ImageFont.truetype('for.ttf', 500)
     w, h = draw.textsize(x, font=font)
     draw.text(((MAX_W - w) / 2, (MAX_H - h) / 2),str(x),(255,255,255),font=font)
     im.save('test.png')
     app.set_profile_photo("test.png")
-    time.sleep(10)
+    time.sleep(30)
